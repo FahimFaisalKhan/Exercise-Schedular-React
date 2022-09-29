7e5690d6bba76820682575f1f75d7a937ebe130c
Fahim <KhanFahimFaisal@outlook.com>
2022-09-29 23:10:10 +0600

    final commit

--- src/App.js
+++ src/App.js
@@ -13,29 +13,31 @@ function App() {
   };
 
   return (
-    <section
-      className="d-flex flex-column flex-md-row"
-      style={{ backgroundColor: "#dfe8eb" }}
-    >
-      <div className="container my-5">
-        <header className="fw-bold fs-1 text-primary">
-          {" "}
-          <FontAwesomeIcon className="me-1" icon={faDumbbell} /> Power Hour
-        </header>
-        <main className="cards-container my-5">
-          <h1 className="fw-semibold fs-3 mb-4 text-secondary ">
-            What Exercices do you prefer today?
-          </h1>
-          <Cards handler={handleTime}></Cards>
-        </main>
-        <section>
-          <Question></Question>
+    <div style={{ backgroundColor: "#dfe8eb" }}>
+      {" "}
+      <section className="d-flex flex-column flex-md-row">
+        <div className="container my-5">
+          <header className="fw-bold fs-1 text-primary">
+            {" "}
+            <FontAwesomeIcon className="me-1" icon={faDumbbell} /> Power Hour
+          </header>
+          <main className="cards-container my-5">
+            <h1 className="fw-semibold fs-3 mb-4 text-secondary ">
+              What Exercices do you prefer today?
+            </h1>
+            <Cards handler={handleTime}></Cards>
+          </main>
+        </div>
+        <section className="cart-sec" style={{ width: "22rem" }}>
+          <div className="cart-container">
+            <Cart time={time}></Cart>
+          </div>
         </section>
-      </div>
-      <div className="cart-container">
-        <Cart time={time}></Cart>
-      </div>
-    </section>
+      </section>
+      <section className="questions">
+        <Question></Question>
+      </section>
+    </div>
   );
 }
 
--- src/components/Buttons/ButtonPrimary.js
+++ src/components/Buttons/ButtonPrimary.js
@@ -5,7 +5,7 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const ButtonPrimary = ({ handler, time }) => {
   return (
-    <div className="d-grid gap-2 ">
+    <div className="d-grid  gap-2  ">
       <Button onClick={() => handler(time)} variant="primary" size="md">
         <FontAwesomeIcon className="me-1" icon={faPlus} /> Block level button
       </Button>
